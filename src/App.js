bc62d0cd5d277203bb47cd324b59dbb8c58b1b52
Gopal Gupta <rggupta225@gmail.com>
2021-04-01 08:46:42 +0530

    manipulating the state by onclick a button

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Person-1', age: 22},
+      {name: 'Person-2', age: 23},
+      {name: 'Person-3', age: 24},
+    ]
+  }
+
+  // changeDetails() { --- } will not work
+  changeDetails = () => {
+    this.setState({
+      persons: [
+        {name: 'Person-5', age: 32},
+        {name: 'Person-6', age: 43},
+        {name: 'Person-7', age: 54},
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>I am react app</h1>
-        <Person name="Person1" salary="5000" > And I work somewhere </Person>
-        <Person name="Person2" salary="6000" />
-        <Person name="Person3" salary="7000" />
+        <button onClick={this.changeDetails}>Click to remove</button>
+        <Person name={this.state.persons[0].name} salary={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} salary={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} salary={this.state.persons[2].age} />
       </div>
     );
   }
