8c4558c6e1183c4b34450a5d05a604b3efb22125
Gopal Gupta <rggupta225@gmail.com>
2021-04-01 07:33:07 +0530

    new component Person add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>I am react app</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'this is the create element'));
+    return (
+      <div className="App">
+        <h1>I am react app</h1>
+        <Person />
+      </div>
+    );
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+export default function Person() {
+    return <p>I am a person component</p>
+}
