1cc6084836bb2c796e29b989c458bd4511cb8259
Gopal Gupta <rggupta225@gmail.com>
2021-04-01 09:43:02 +0530

    useState() Hook for state Manipultaion - 2 with multiple setStates

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,25 +1,46 @@
 import React, { useState } from 'react';
 
+let visible = true;
 export default function Person(props) {
+    let person = {
+        name: props.name,
+        age: props.age,
+    };
+    let personNoDetails = {
+        name: '*******',
+        age: '***',
+    }
+
     const [currentState, updateState] = useState({
-        person: {
-            name: props.name,
-            age: props.age,
-        }
+        person: person
     });
 
-    const disappearName = () => {
-        updateState({
-            person: {
-                name: '*******',
-                age: '***',
-            }
-        })
+    const [visiblityState, updateVisiblityState] = useState({
+        visiblity: 'Hide Details'
+    });
+
+    const changeVisiblity = () => {
+        if(visible){
+            updateState({
+                person: personNoDetails 
+            })
+            updateVisiblityState({
+                visiblity: 'Show Details'
+            })
+        } else {
+            updateState({
+                person: person 
+            })
+            updateVisiblityState({
+                visiblity: 'Hide Details'
+            })
+        }
+        visible = !(visible);
     }
 
     return (
         <div>
-            <p>I am {currentState.person.name} component and age is {currentState.person.age} || <b onClick={disappearName}>Click here</b></p>
+            <p>I am {currentState.person.name} component and age is {currentState.person.age} || <b onClick={changeVisiblity}>{visiblityState.visiblity}</b></p>
         </div>
     )
 }
